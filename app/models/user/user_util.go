9ee275f99c13e2738554a04263eba46b90c881da
路西法 <zhaiyuxin103@hotmail.com>
2023-07-27 22:52:36 +0800

    feat(api): 通过邮箱找回密码

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -43,3 +43,9 @@ func Get(id string, loading bool) (user User) {
 	}
 	return
 }
+
+// GetByEmail 通过 Email 来获取用户
+func GetByEmail(email string) (userModel User) {
+	database.DB.Where("email = ?", email).First(&userModel)
+	return
+}
