ee769fe8a0622ac3d222b806d69a8ee8e1227e0e
路西法 <zhaiyuxin103@hotmail.com>
2023-08-07 13:25:55 +0800

    perf(main): API 域名和前缀

--- routes/api.go
+++ routes/api.go
@@ -6,6 +6,7 @@ import (
 	api "server/app/http/controllers/api/v1"
 	"server/app/http/controllers/api/v1/auth"
 	"server/app/http/middlewares"
+	"server/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,97 +15,101 @@ import (
 func RegisterAPIRoutes(r *gin.Engine) {
 
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-	v1 := r.Group("/v1")
-	{
-		// 注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			// 以 JSON 格式响应
-			c.JSON(http.StatusOK, gin.H{
-				"status":  "success",
-				"code":    http.StatusOK,
-				"message": "Hello API.",
-				"data":    gin.H{},
-				"error":   gin.H{},
-			})
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
+
+	// 注册一个路由
+	v1.GET("/", func(c *gin.Context) {
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"code":    http.StatusOK,
+			"message": "Hello API.",
+			"data":    gin.H{},
+			"error":   gin.H{},
 		})
+	})
 
-		// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
-		// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
-		// 测试时，可以调高一点。
-		v1.Use(middlewares.LimitIP("200-H"))
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
 
+	{
+		authGroup := v1.Group("/auth")
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
+		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
-			authGroup := v1.Group("/auth")
-			// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
-			// 测试时，可以调高一点
-			authGroup.Use(middlewares.LimitIP("1000-H"))
+			// 注册用户
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", middlewares.GuestJWT(), suc.IsPhoneExist)
+			// 判断 Email 是否已注册
+			authGroup.POST("/signup/email/exist", middlewares.GuestJWT(), suc.IsEmailExist)
+			authGroup.POST("/signup/using-phone", middlewares.GuestJWT(), suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-email", middlewares.GuestJWT(), suc.SignupUsingEmail)
+
+			// 发送验证码
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("20-H"), vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("50-H"), vcc.SendUsingEmail)
+
+			// 登录
+			lgc := new(auth.LoginController)
+			// 使用手机号，短信验证码进行登录
+			authGroup.POST("/login/using-phone", middlewares.GuestJWT(), lgc.LoginByPhone)
+			// 支持手机号 和 Email
+			authGroup.POST("/login/using-password", middlewares.GuestJWT(), lgc.LoginByPassword)
+			authGroup.POST("/login/refresh-token", middlewares.AuthJWT("api"), lgc.RefreshToken)
+
+			// 重置密码
+			pwc := new(auth.PasswordController)
+			authGroup.POST("/password-reset/using-phone", middlewares.GuestJWT(), pwc.ResetByPhone)
+			authGroup.POST("/password-reset/using-email", middlewares.GuestJWT(), pwc.ResetByEmail)
+
+			uc := new(api.UsersController)
+
+			// 获取当前用户
+			v1.GET("/user", middlewares.AuthJWT("api"), uc.CurrentUser)
+			usersGroup := v1.Group("/users")
 			{
-				// 注册用户
-				suc := new(auth.SignupController)
-				// 判断手机是否已注册
-				authGroup.POST("/signup/phone/exist", middlewares.GuestJWT(), suc.IsPhoneExist)
-				// 判断 Email 是否已注册
-				authGroup.POST("/signup/email/exist", middlewares.GuestJWT(), suc.IsEmailExist)
-				authGroup.POST("/signup/using-phone", middlewares.GuestJWT(), suc.SignupUsingPhone)
-				authGroup.POST("/signup/using-email", middlewares.GuestJWT(), suc.SignupUsingEmail)
+				usersGroup.GET("", uc.Index)
+				usersGroup.PUT("", middlewares.AuthJWT("api"), uc.Update)
+				usersGroup.PATCH("/email", middlewares.AuthJWT("api"), uc.UpdateEmail)
+				usersGroup.PATCH("/phone", middlewares.AuthJWT("api"), uc.UpdatePhone)
+				usersGroup.PATCH("/password", middlewares.AuthJWT("api"), uc.UpdatePassword)
+			}
 
-				// 发送验证码
-				vcc := new(auth.VerifyCodeController)
-				// 图片验证码，需要加限流
-				authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("20-H"), vcc.ShowCaptcha)
-				authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), vcc.SendUsingPhone)
-				authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("50-H"), vcc.SendUsingEmail)
+			cgc := new(api.CategoriesController)
+			cgcGroup := v1.Group("/categories")
+			{
+				cgcGroup.GET("", cgc.Index)
+				cgcGroup.POST("", middlewares.AuthJWT("api"), cgc.Store)
+				cgcGroup.PUT("/:id", middlewares.AuthJWT("api"), cgc.Update)
+				cgcGroup.DELETE("/:id", middlewares.AuthJWT("api"), cgc.Delete)
+			}
 
-				// 登录
-				lgc := new(auth.LoginController)
-				// 使用手机号，短信验证码进行登录
-				authGroup.POST("/login/using-phone", middlewares.GuestJWT(), lgc.LoginByPhone)
-				// 支持手机号 和 Email
-				authGroup.POST("/login/using-password", middlewares.GuestJWT(), lgc.LoginByPassword)
-				authGroup.POST("/login/refresh-token", middlewares.AuthJWT("api"), lgc.RefreshToken)
+			tpc := new(api.TopicsController)
+			tpcGroup := v1.Group("/topics")
+			{
+				tpcGroup.GET("", tpc.Index)
+				tpcGroup.POST("", middlewares.AuthJWT("api"), tpc.Store)
+				tpcGroup.GET("/:id", tpc.Show)
+				tpcGroup.PUT("/:id", middlewares.AuthJWT("api"), tpc.Update)
+				tpcGroup.DELETE("/:id", middlewares.AuthJWT("api"), tpc.Delete)
+			}
 
-				// 重置密码
-				pwc := new(auth.PasswordController)
-				authGroup.POST("/password-reset/using-phone", middlewares.GuestJWT(), pwc.ResetByPhone)
-				authGroup.POST("/password-reset/using-email", middlewares.GuestJWT(), pwc.ResetByEmail)
-
-				uc := new(api.UsersController)
-
-				// 获取当前用户
-				v1.GET("/user", middlewares.AuthJWT("api"), uc.CurrentUser)
-				usersGroup := v1.Group("/users")
-				{
-					usersGroup.GET("", uc.Index)
-					usersGroup.PUT("", middlewares.AuthJWT("api"), uc.Update)
-					usersGroup.PATCH("/email", middlewares.AuthJWT("api"), uc.UpdateEmail)
-					usersGroup.PATCH("/phone", middlewares.AuthJWT("api"), uc.UpdatePhone)
-					usersGroup.PATCH("/password", middlewares.AuthJWT("api"), uc.UpdatePassword)
-				}
-
-				cgc := new(api.CategoriesController)
-				cgcGroup := v1.Group("/categories")
-				{
-					cgcGroup.GET("", cgc.Index)
-					cgcGroup.POST("", middlewares.AuthJWT("api"), cgc.Store)
-					cgcGroup.PUT("/:id", middlewares.AuthJWT("api"), cgc.Update)
-					cgcGroup.DELETE("/:id", middlewares.AuthJWT("api"), cgc.Delete)
-				}
-
-				tpc := new(api.TopicsController)
-				tpcGroup := v1.Group("/topics")
-				{
-					tpcGroup.GET("", tpc.Index)
-					tpcGroup.POST("", middlewares.AuthJWT("api"), tpc.Store)
-					tpcGroup.GET("/:id", tpc.Show)
-					tpcGroup.PUT("/:id", middlewares.AuthJWT("api"), tpc.Update)
-					tpcGroup.DELETE("/:id", middlewares.AuthJWT("api"), tpc.Delete)
-				}
-
-				lsc := new(api.LinksController)
-				linksGroup := v1.Group("/links")
-				{
-					linksGroup.GET("", lsc.Index)
-				}
+			lsc := new(api.LinksController)
+			linksGroup := v1.Group("/links")
+			{
+				linksGroup.GET("", lsc.Index)
 			}
 		}
 	}
